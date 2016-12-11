30f6b22504253e9e782374e68e6129aa4842c541
patyk <patyk@DESKTOP-RHDUJH3>
2016-12-11 13:31:31 +0100

    Add project files.

---
+++ WebApplication2/Controllers/Value2Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Xama.Portable;
+
+namespace WebApplication2.Controllers {
+
+	public class Value2Controller : ApiController {
+		// GET api/value2
+		
+
+		public IEnumerable<TaskItem> Get() {
+			var baza = new TaskDBCtxt();
+			return baza.TaskItems.ToList();
+
+		}
+
+		// GET api/values/5
+		public string Get(int id) {
+			return "value";
+		}
+
+		// POST api/values
+		public void Post([FromBody] string value) {
+		}
+
+		// PUT api/values/5
+		public void Put(int id, [FromBody] string value) {
+		}
+
+		// DELETE api/values/5
+		public void Delete(int id) {
+		}
+	}
+
+}
---
+++ WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Xama.Portable;
+
+namespace WebApplication2.Controllers {
+
+	public class ValuesController : ApiController {
+		// GET api/values
+		public List<TaskItem> Tasks { get; set; }
+
+		public IEnumerable<User> Get() {
+			
+				var baza = new UserDBCtxt();
+			
+			return baza.Users.ToList();
+		}
+
+		
+
+		// GET api/values/5
+		public string Get(int id) {
+			return "value";
+		}
+
+		// POST api/values
+		public void Post([FromBody] string value) {
+		}
+
+		// PUT api/values/5
+		public void Put(int id, [FromBody] string value) {
+		}
+
+		// DELETE api/values/5
+		public void Delete(int id) {
+		}
+	}
+
+}
